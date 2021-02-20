6bca78bcb2690487808eb0a85de1a24640bd7004
Dev <ursprajwal1@gmail.com>
2021-02-20 14:08:07 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+  console.log(' ');
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Sever running on port ${PORT}`));
